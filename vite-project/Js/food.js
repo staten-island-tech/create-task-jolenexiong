3f417647773a480b23423306ff2e0d6e67f0dba4
jolenexiong <144476558+jolenexiong@users.noreply.github.com>
2024-01-12 12:40:13 -0500

    Update food.js

--- vite-project/Js/food.js
+++ vite-project/Js/food.js
@@ -1,5 +1,6 @@
 food = [
 {
     name:  "Pizza",
+    img: "https://images.ctfassets.net/nw5k25xfqsik/64VwvKFqxMWQORE10Tn8pY/200c0538099dc4d1cf62fd07ce59c2af/20220211142754-margherita-9920.jpg"
 } 
 ]
