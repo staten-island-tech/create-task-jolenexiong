35d440736ccadc8b616dd57ed6af84b1cb011ad5
jolenexiong <144476558+jolenexiong@users.noreply.github.com>
2024-01-12 13:14:37 -0500

    ssss

--- vite-project/Js/main.js
+++ vite-project/Js/main.js
@@ -1,2 +1,13 @@
-import "../css/style.css";
-import { food } from "./food";
+import "./css/style.css";
+
+const URL = "https://simple-grocery-store-api.glitch.me"
+
+async function getData(URL) {
+  const reponse = await fetch(URL);
+  console.log(reponse);
+  let result= await fetch ("https://southparkquotes.onrender.com/v1/quotes/search/cartman");
+let data = await result.json();
+console.log(data);
+}
+
+getData(URL);
