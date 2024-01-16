6ff380c169ea2687891f32ddb58440f0bc24e6a8
jolenexiong <144476558+jolenexiong@users.noreply.github.com>
2024-01-16 12:40:24 -0500

    Update main.js

--- vite-project/Js/main.js
+++ vite-project/Js/main.js
@@ -5,7 +5,7 @@ const URL = "https://simple-grocery-store-api.glitch.me"
 async function getData(URL) {
   const reponse = await fetch(URL);
   console.log(reponse);
-  let result= await fetch ("https://southparkquotes.onrender.com/v1/quotes/search/cartman");
+  let result= await fetch ("GET /products");
 let data = await result.json();
 console.log(data);
 }
