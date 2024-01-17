0500cace6da599d9819bbc9952588a369c3d92d7
jolenexiong <144476558+jolenexiong@users.noreply.github.com>
2024-01-17 12:39:40 -0500

    Update main.js

--- vite-project/Js/main.js
+++ vite-project/Js/main.js
@@ -3,17 +3,14 @@ import "./css/style.css";
 const URL = "https://api.punkapi.com/v2/beers/random"
 
 async function getData(URL) {
-  const reponse = await fetch(URL);
-  console.log(reponse);
-  let result= await fetch ("GET /products");
-let data = await result.json();
-console.log(data);
+try{
+  const
 }
 
 getData(URL);
 
 const DOMSelectors = {
   searchbtn: document.querySelector(".searchbtn"),
-  allergenbtn: document.querySelector(".serachallergenbtn"),
+  allergenbtn: document.querySelector(".allergenbtn"),
 }
 
