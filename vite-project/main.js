7c2f3ab48b02a6533b4a05a9a0917789ca14693f
jolenexiong <144476558+jolenexiong@users.noreply.github.com>
2024-01-18 13:15:05 -0500

    ?

--- vite-project/main.js
+++ vite-project/main.js
@@ -2,11 +2,15 @@ import "./css/style.css";
 
 const URL = "https://api.punkapi.com/v2/beers/random"
 
+
+let bottles= null;
+
 async function getData(URL) {
 try{
   const res = await fetch(URL);
   const data = await res.json();
   console.log(data);
+  bottles = data;
 }
 catch (error){
   console.log(error);
@@ -22,12 +26,14 @@ const DOMSelectors = {
 }
 
 function displayall() {
-  BeforeUnloadEvent.forEach((bottle)=> AudioParamMap.insertAdjacentHTML(
+  beer.forEach((bottle)=> DOMSelectors.gallery.insertAdjacentHTML(
     "beforeend",
     `<div class="card">
 <h2 class="name">${bottle.name}</h2>
 <img src = "${bottle.img_url}"class="pic">
-<h2>${bottle.food_pairing}</h2>
+<h2>${bottle.tagline}</h2>
 </div>`
 ));
 }
+
+displayall();
