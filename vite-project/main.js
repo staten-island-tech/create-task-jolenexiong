5e4ee4160aed87e137807703b3d256d1150620bc
jolenexiong <144476558+jolenexiong@users.noreply.github.com>
2024-01-22 13:13:17 -0500

    god help m

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,47 +1,41 @@
 import "./css/style.css";
 
-const URL = "https://api.punkapi.com/v2/beers/random"
+const URL = "https://api.punkapi.com/v2/beers"
 
-
-let bottles= null;
-
-async function getData(URL) {
-try{
-  const res = await fetch(URL);
-  const data = await res.json();
-  console.log(data);
-  bottles = data;
-}
-catch (error){
-  console.log(error);
-}
-};
-
-getData(URL);
-
-
-async functionAddData () {
-  const gallery = document.querySelector('.gallery');
-  const data = await getData();
-
-  data.forEach(beer=> {
-    const card = document.createElement('div');
-    card.classList.add('card');
-
-    const name = document.createElement('h1');
-    name.textContent = beer.name;
-
-    const desc = document.createElement('h2');
-    desc.textContent = beer.description;
-
-    const attnlv = document.createElement('h3');
-    attnlv.textContent = 
-  })
-}
+// async function getData(URL) {
+// try{
+//   const res = await fetch(URL);
+//   const data = await res.json();
+//   console.log(data);
+//   return(data);
+  
+// }
+// catch (error){
+//   console.log(error);
+// }
+// };
+// getData(URL);
 
 const DOMSelectors = {
   searchbtn: document.querySelector(".searchbtn"),
   allergenbtn: document.querySelector(".allergenbtn"),
   gallery: document.querySelector(".gallery"),
+  search: document.querySelector("#lookbeer"),
 }
+
+async function getData () {
+  try{
+    const res = await fetch(URL);
+    const beer = await res.json();
+    console.log(beer);
+    return(beer);
+  }
+  catch (error){
+    console.log(error);
+  
+}}
+
+getData()
+
+
 
