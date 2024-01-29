ae946ec3c5b3a78b600f34fdaf6d581da7e8e2b8
jolenexiong <144476558+jolenexiong@users.noreply.github.com>
2024-01-29 12:56:44 -0500

    html but no filters

--- vite-project/main.js
+++ vite-project/main.js
@@ -26,16 +26,36 @@ const DOMSelectors = {
 async function getData () {
   try{
     const res = await fetch(URL);
-    const beer = await res.json();
-    console.log(beer);
-    return(beer);
+    const beers = await res.json();
+    console.log(beers);
+    addCard(beers);
+    return(beers);
   }
   catch (error){
     console.log(error);
   
-}}
-
+}};
 getData()
+
+function addCard (beers) {
+  beers.forEach((beer) =>
+  DOMSelectors.gallery.insertAdjacentHTML (
+    "beforeend", 
+    `<div class="card">
+        <div class="name">${beer.name}</div>
+        <div class="tagline">${beer.tagline}</div>
+        <div class="img">${beer.img_url}</div>
+        <div class="description">${beer.description}</div>
+        <div class="attenuation">${beer.attenuation_level}</div>
+        
+      </div>`
+    
+  ))
+};
+
+
+
+
 
 
 
