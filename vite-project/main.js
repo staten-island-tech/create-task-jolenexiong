6405a1521155e3159368c7424a997a6cdbc052f3
jolenexiong <144476558+jolenexiong@users.noreply.github.com>
2024-01-31 22:15:51 -0500

    COMPLETED!

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,27 +1,18 @@
 import "./css/style.css";
 const URL = "https://api.potterdb.com/v1/spells"
-// async function getData(URL) {
-// try{
-//   const res = await fetch(URL);
-//   const data = await res.json();
-//   console.log(data);
-//   return(data);
-  
-// }
-// catch (error){
-//   console.log(error);
-// }
-// };
-// getData(URL);
+
 const DOMSelectors = {
   searchbtn: document.querySelector(".searchbtn"),
   gallery: document.querySelector(".gallery"),
   search: document.querySelector("#lookspells"),
   dropdown: document.getElementById('dropdown'),
+  showallbtn: document.querySelector(".showall"),
+  errormessage: document.querySelector(".errormessage"),
+  removebtn: document.querySelector(".removebtn"),
 };
 let allSpells = {};
-async function getData () {
-  try{
+async function getData() {
+  try {
     const res = await fetch(URL);
     const spells = await res.json();
     console.log(spells);
@@ -29,50 +20,64 @@ async function getData () {
     addCard(allSpells);
     return spells;
   }
-  catch (error){
+  catch (error) {
     console.log(error);
-  
-}
+
+  }
 };
 getData();
-function addCard (spells) {
+
+function addCard(spells) {
   spells.forEach((spell) =>
-  DOMSelectors.gallery.insertAdjacentHTML (
-    "beforeend", 
-    `<div class="card">
+    DOMSelectors.gallery.insertAdjacentHTML(
+      "beforeend",
+      `<div class="card">
         <div class="name">${spell.attributes.name}</div>
         <div class="img"><img src="${spell.attributes.image}"></div>
         <div class="effect">Effect:${spell.attributes.effect}</div>
-        <div class="hand">Hand Movements:${spell.attributes.hand}</div>
-        
       </div>`
-  ))
+    ))
 };
 
-function removeall () {
+function removeall() {
   DOMSelectors.gallery.innerHTML = "";
 }
 
-// DOMSelectors.dropdown.addEventListener('click', function(event){
-//   event.preventDefault();
-//   removeall();
-//   const spellcategory = DOMSelectors.dropdown.value.toLowerCase();
-//   const filtered = allSpells.filter((spell)=> 
-//   spell.attributes.category.toLowerCase()=== spellcategory
-//   );
-//   addCard(filtered);
-// });
-
-DOMSelectors.dropdown.addEventListener('change', function() {
+DOMSelectors.dropdown.addEventListener('change', function () {
   const spellcategory = DOMSelectors.dropdown.value;
-  console.log('Selected Category:', spellcategory);
-  
+  console.log('selected category:', spellcategory);
+
   const filtered = allSpells.filter((spell) =>
     spell.attributes.category === spellcategory
   );
-  
-  console.log('Filtered Spells:', filtered);
-  
+
+  console.log('filtered:', filtered);
+
   removeall();
   addCard(filtered);
 });
+
+DOMSelectors.showallbtn.addEventListener('click', function (event) {
+  event.preventDefault();
+  addCard(allSpells);
+})
+
+DOMSelectors.searchbtn.addEventListener('click', function (event) {
+  event.preventDefault();
+  removeall();
+
+  const search = DOMSelectors.search.value.toLowerCase();
+  const spellmatch = allSpells.filter((spell) => spell.attributes.name.toLowerCase() === search)
+
+if (spellmatch.length > 0) {
+  addCard(spellmatch);
+  DOMSelectors.errormessage.textContent = ""
+} else {
+  DOMSelectors.errormessage.textContent = "Sorry that spell doesn't exist."
+}
+});
+
+DOMSelectors.removebtn.addEventListener('click', function (event){
+  event.preventDefault();
+  removeall();
+})
